ce07a98f03f31f4a18ade821b8f9e1a5546cbc4d
SeokYoung Shin <aceorb@gmail.com>
2019-01-15 22:14:48 +0900

    Add some util functions.

--- datetime.js
+++ datetime.js
@@ -10,7 +10,27 @@ var getUnixTimeStampSec = () => {
 
 
 
+
+var getDateVal = function(date_obj) {
+
+    if(typeof date_obj == 'undefined' || date_obj == null) {
+        date_obj = new Date();
+    }
+
+    var y = date_obj.getFullYear().toString(); 
+
+    var m = (date_obj.getMonth()+1).toString();
+    if(m.length < 2) m = "0"+m;
+
+    var d = (date_obj.getDate()).toString();
+    if(d.length < 2) d = "0"+d;
+
+    return y+m+d;
+}
+
+
 module.exports = {
     getUnixTimeStampMsec : getUnixTimeStampMsec,
-    getUnixTimeStampSec : getUnixTimeStampSec
+    getUnixTimeStampSec : getUnixTimeStampSec,
+    getDateVal : getDateVal
 }
--- util.js
+++ util.js
@@ -1,3 +1,10 @@
+require('magic-globals');
+require('console-stamp')(console, '[yyyy-mm-dd HH:MM:ss.l]');
+
+
+
+
+
 var isNullOrEmpty = (x) => {
     if(typeof x == 'undefined') return true;
     if(x == null) return true;
@@ -38,9 +45,90 @@ var trace = (filename, line, msg) =>{
 
 
 
+
+var asyncSleep = function(msec) {
+    return new Promise((resolve) =>{
+        setTimeout(()=>{
+            resolve();
+        }, msec);
+    });
+}
+
+
+
+
+var syncSleep = function(msec) {
+    if(msec <= 0) return;
+    var st = new Date().getTime();
+    for(;;) {
+        dt = new Date().getTime() - st;
+        if(dt >= msec) break;
+    }
+}
+
+
+
+
+
+
+var isPrivateIP = function(ip_str) {
+
+    var tok = ip_str.split(".");
+    if(tok.length != 4) return false;
+
+    if(tok[0] == 10)
+    {
+        if( tok[1] >= 0 && tok[1] <= 255 &&
+            tok[2] >= 0 && tok[2] <= 255 &&
+            tok[3] >= 0 && tok[3] <= 255) return true;
+    }
+    else if(tok[0] == 172)
+    {
+        if( tok[1] >= 16 && tok[1] <= 31 &&
+            tok[2] >= 0  && tok[2] <= 255 &&
+            tok[3] >= 0  && tok[3] <= 255) return true;
+    }
+    else if(tok[0] == 192)
+    {
+        if( tok[1] == 168 &&
+            tok[2] >= 0   && tok[2] <= 255 &&
+            tok[3] >= 0   && tok[3] <= 255) return true;
+    }
+
+    return false;
+}
+
+
+
+
+
+var getPrivateIPList = function() {
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                if(isPrivateIP(address.address) == true) addresses.push(address.address);
+            }
+        }
+    }
+
+    return addresses;
+}
+
+
+
+
+
 module.exports = {
     isNullOrEmpty : isNullOrEmpty,
     valueWithFallback : valueWithFallback,
     isValidPort : isValidPort,
-    trace : trace
+    trace : trace,
+    asyncSleep : asyncSleep,
+    syncSleep : syncSleep,
+    isPrivateIP : isPrivateIP,
+    getPrivateIPList : getPrivateIPList
 }
